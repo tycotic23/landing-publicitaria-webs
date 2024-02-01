074da2f256efa354da4ce5cdc1682010fe57ef3e
tycotic23 <magui.maggie@gmail.com>
2024-02-01 01:41:58 +0000

    boton redondo y textura

--- index.js
+++ index.js
@@ -8,3 +8,32 @@ menuButton.addEventListener('click',()=>{
 menuDiv.addEventListener('click',()=>{
     menuDiv.classList.toggle('nav-hidden');
 });
+
+/* girar tarjetas */
+
+let cards=document.querySelectorAll(".recuadro__item-card");
+cards.forEach(card=>{
+    card.addEventListener('click',()=>{
+        card.classList.toggle("recuadro__item-card-flip");
+    });
+});
+
+
+/* seccion de proceso */
+let heros=document.querySelectorAll(".container-hero__column");
+
+function desactivarHeros(){
+    heros.forEach(hero=>{
+        hero.classList.remove('active');
+    });
+}
+
+heros.forEach(hero=>(
+    hero.addEventListener('click',()=>{
+        /* desactivamos a los demas */
+        desactivarHeros();
+        /* activamos al seleccionado */
+        hero.classList.add('active');
+    })
+));
+
